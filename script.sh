2cde7dfcd27a12e6cd3e339fb81ca39d46b29273
Kalyani7744 <83349665+Kalyani7744@users.noreply.github.com>
2021-11-24 12:23:21 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,47 @@
+
+function mysql()
+{
+#Install MySQL
+sudo apt update
+sudo apt install -y mysql-server 
+}
+
+function postgre()
+{
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+sudo apt update
+sudo apt-get -y install postgresql postgresql-contrib
+}
+
+function neo4j()
+{
+#Install Neo4j
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://debian.neo4j.com stable 4.1"
+sudo apt -y install neo4j
+sudo systemctl enable neo4j.service
+sudo systemctl status neo4j.service
+}
+
+function dynamodb()
+{
+#install DynamoDB
+mkdir ./dynamolocal
+cd ./dynamolocal
+wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz
+tar xzf dynamodb_local_latest.tar.gz
+}
+
+mysql
+
+postgre
+
+neo4j
+
+dynamodb
+
+chmod u+x script.sh
+
