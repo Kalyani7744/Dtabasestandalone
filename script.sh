fa0ae8997b984826a11c0e4d80cbb113a0949ddd
Kalyani7744 <83349665+Kalyani7744@users.noreply.github.com>
2021-12-02 11:34:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,13 +34,25 @@ wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_lat
 tar xzf dynamodb_local_latest.tar.gz
 }
 
+function mongodb()
+{
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+apt-key list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl enable mongod
+sudo systemctl start mongod
+}
+
 mysql
 
 postgre
 
 neo4j
 
-dynamodb
+mongodb
 
 
 
