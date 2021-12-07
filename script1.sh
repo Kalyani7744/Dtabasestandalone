b2a646c9a89fc1871a487fd646db99a822fc8262
Kalyani7744 <83349665+Kalyani7744@users.noreply.github.com>
2021-12-07 19:00:11 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install curl
 sudo apt-get install apt-transport-https 
-sudo apt install -y virtualbox virtualbox-ext-pack
+sudo apt-get -y install virtualbox
 wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo cp minikube-linux-amd64 /usr/local/bin/minikube
 sudo chmod 755 /usr/local/bin/minikube
