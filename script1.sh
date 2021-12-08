b88a0ef1498c5f88ee34a0d20847943475951582
Kalyani7744 <83349665+Kalyani7744@users.noreply.github.com>
2021-12-08 15:42:22 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,17 +1,16 @@
 #! /bin/sh
 function minikube()
 {
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install apt-transport-https 
-sudo apt-get -y install virtualbox
-sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo cp minikube-linux-amd64 /usr/local/bin/minikube
-sudo chmod 755 /usr/local/bin/minikube
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get upgrade
+sudo apt -y install virtualbox virtualbox-ext-pack
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-minikube start
 }
 
 minikube
