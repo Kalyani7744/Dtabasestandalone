3f1656412f4748561b544f8df89fc90b0c15b9bf
Kalyani7744 <83349665+Kalyani7744@users.noreply.github.com>
2021-12-08 15:03:54 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -12,7 +12,6 @@ sudo chmod 755 /usr/local/bin/minikube
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version -o json
 minikube start
 }
 
